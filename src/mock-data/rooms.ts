70e1d886327c1798f0b8a4f08d709e4d21ab68b0
Filip Sauer <sauer.filip@gmail.com>
2022-11-04 20:23:11 +0100

    add mock-data

--- src/mock-data/rooms.ts
+++ src/mock-data/rooms.ts
@@ -1,9 +1,17 @@
-export interface Rooms {
-  dupa: string;
+export interface Room {
+  buildingId: string;
+  roomNames: string[];
+  level: string;
+  x: number;
+  y: number;
 }
 
-export const rooms: Rooms[] = [
+export const rooms: Room[] = [
   {
-    dupa: "dupa",
+    buildingId: "A-1",
+    roomNames: ["100", "L100"],
+    level: "0",
+    x: 0,
+    y: 0,
   },
 ];
