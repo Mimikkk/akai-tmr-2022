14dd70cedaf22fb5135441f76aa7c80bc04d4550
Daniel Zdancewicz <d.zdancewicz@teb-akademia.pl>
2022-11-04 22:26:51 +0100

    serach

--- src/services/building.service.tsx
+++ src/services/building.service.tsx
@@ -1,9 +1,26 @@
 import supabase from "../supabase";
 
-export const BuildingService = {
-  getAll: async () => {
-    const data = await supabase.from("buildings").select("*");
+const toBuilding = ({ id, name, lat, lon }: any) => ({
+  id: id,
+  names: [name],
+  geolocation: {
+    latitude: lat,
+    longitude: lon,
+  },
+});
 
-    console.log(data);
+export const BuildingService = {
+  readAll: async () => {
+    const { data, error } = await supabase.from("buildings").select();
+    if (error) throw error;
+    return data.map(toBuilding);
+  },
+  search: async (query: string) => {
+    const { data }: any = await supabase
+      .from("buildings")
+      .select("*")
+      .like("name", `%${query}%`);
+
+    return data.map(toBuilding);
   },
 };
