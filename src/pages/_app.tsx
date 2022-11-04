fd263c10a57c26d778707c6b313b3dea9562bdb2
Daniel Zdancewicz <d.zdancewicz@teb-akademia.pl>
2022-11-04 21:23:36 +0100

    confetti

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,17 @@ import "../../styles/globals.css";
 import type { AppProps } from "next/app";
 import s from "./_app.module.scss";
 import { Icon } from "../components";
+import { useState } from "react";
+import Confetti from "react-confetti";
+import { useWindowSize } from "react-use";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [showConfetti, toggleConfetti] = useState(false);
+  const { width, height } = useWindowSize();
+
   return (
-    <div className={s.app}>
+    <div className={s.app} onClick={() => toggleConfetti(true)}>
+      {showConfetti && <Confetti width={width} height={height} />}
       <span className={s.header}>
         <Icon name="ArrowRight" />
         <span className={s.title}>Jest zajebiście</span>
