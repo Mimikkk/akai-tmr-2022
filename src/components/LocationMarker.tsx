ec9a7161cde2faf465662c534962c1219e3e5b20
rpoznanski <r.poznanski@teb-akademia.pl>
2022-11-05 10:05:59 +0100

    location marker content

--- src/components/LocationMarker.tsx
+++ src/components/LocationMarker.tsx
@@ -26,14 +26,8 @@ export const LocationMarker = () => {
 
   return position === null ? null : (
     <Marker position={position}>
-      <Popup>
-        You are here. <br />
-        Map bbox: <br />
-        <b>Southwest lng</b>: {bbox[0]} <br />
-        <b>Southwest lat</b>: {bbox[1]} <br />
-        <b>Northeast lng</b>: {bbox[2]} <br />
-        <b>Northeast lat</b>: {bbox[3]}
-      </Popup>
+      <Popup>Jesteś tutaj</Popup>
     </Marker>
   );
 };
+
