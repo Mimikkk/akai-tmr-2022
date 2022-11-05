699dd2511c2ca9a8eddf4991a31e64d9f39fcf8a
Jerzor <jerzor2000@vp.pl>
2022-11-05 02:34:11 +0100

    add create building function in service

--- src/services/building.service.tsx
+++ src/services/building.service.tsx
@@ -1,7 +1,19 @@
 import { Building } from "../models";
 import axios from "axios";
+import supabase from "../supabase";
+
+type BuildingProps = Omit<Building, "rooms">;
 
 export const BuildingService = {
   search: async (query: string): Promise<Building[]> =>
     await axios.get("/api/search", { params: { query } }).then((res) => res.data),
+
+  create: async ({ name, longitude, latitude, displayName, aliases }: BuildingProps) => {
+    const { data: buildings, error }: any = await supabase
+      .from("buildings")
+      .insert([{ name, longitude, latitude, displayName, aliases }]);
+
+    if (error) throw error;
+    return buildings;
+  },
 };
