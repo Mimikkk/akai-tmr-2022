1e5a9729bccc1c10ad202a86aceb3bd5395e6bb4
Daniel Zdancewicz <d.zdancewicz@teb-akademia.pl>
2022-11-05 02:09:46 +0100

    aa

---
+++ src/components/BuildingTile/AddNewRoomCard.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import { Icon } from "../Icon";
+import s from "./AddNewRoomCard.module.scss";
+
+export const AddNewRoomCard = () => (
+  <Link href={"room/add"}>
+    <div className={s.card}>
+      <label htmlFor={"add-new"} className={"cursor-pointer"}>
+        Dodaj nową sale
+      </label>
+      <Icon name={"ArrowRight"} id={"add-new"} />
+    </div>
+  </Link>
+);
