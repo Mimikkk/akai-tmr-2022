f5c00d2c13405a2df102d2f7b73080d6b4b9d96f
Filip Sauer <sauer.filip@gmail.com>
2022-11-05 10:55:49 +0100

    better RoomCard link

--- src/components/BuildingTile/RoomsList/components/RoomCard.tsx
+++ src/components/BuildingTile/RoomsList/components/RoomCard.tsx
@@ -5,6 +5,7 @@ import { Urls } from "../../../../urls";
 import { Room } from "../../../../models";
 import { RoomService } from "../../../../services";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import Link from "next/link";
 
 export interface RoomProps {
   room: Room;
@@ -25,26 +26,28 @@ export const RoomCard = ({ room, className }: RoomProps) => {
   });
 
   return (
-    <li className={cx(s.content, className)}>
-      <div className={s.vote}>
-        <Icon name={"ThumbsUp"} className={s.upvote} onClick={() => addPoint.mutate()} />
-        {room.score}
-        <Icon name={"ThumbsDown"} className={s.downvote} onClick={() => removePoint.mutate()} />
-      </div>
-      <div className={"grow mx-2"}>
-        <h2 className={cx(s.title)}>{room.name}</h2>
-        <ul className={s.rooms}>
-          <span className={s.room} title="Alternatywna nazwa dla sali">
-            Inne nazwy:
-          </span>
-          {room.aliases.map((name) => (
-            <li key={name} className={s.chip}>
-              {name}
-            </li>
-          ))}
-        </ul>
-      </div>
-      <Icon name="ArrowRight" href={`${Urls.Room}/${room.id}`} />
-    </li>
+    <Link href={`${Urls.Room}/${room.id}`}>
+      <li className={cx(s.content, className)}>
+        <div className={s.vote}>
+          <Icon name={"ThumbsUp"} className={s.upvote} onClick={() => addPoint.mutate()} />
+          {room.score}
+          <Icon name={"ThumbsDown"} className={s.downvote} onClick={() => removePoint.mutate()} />
+        </div>
+        <div className={"grow mx-2"}>
+          <h2 className={cx(s.title)}>{room.name}</h2>
+          <ul className={s.rooms}>
+            <span className={s.room} title="Alternatywne nazwy dla sali">
+              Inne nazwy:
+            </span>
+            {room.aliases.map((name) => (
+              <li key={name} className={s.chip}>
+                {name}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <Icon name="ArrowRight" />
+      </li>
+    </Link>
   );
 };
