3599fca7193047996749d8b3ff524f53ff5e41ee
Dominik Kostencki <d.kostencki@teb-akademia.pl>
2022-11-05 01:37:50 +0100

    hhh

--- src/pages/addRoom.tsx
+++ src/pages/addRoom.tsx
@@ -3,7 +3,7 @@ import { Input } from "../components";
 const AddBuildingPage = () => {
   return (
     <main className={"bg-slate-500 w-full flex justify-center p-2"}>
-      <section>
+      <section className={"[&>*]:my-4"}>
         <Input icon={"Room"}> Nazwa sali </Input>
         <Input icon={"Building"}> Budynek </Input>
         <Input icon={"Level"}> Piętro </Input>
