d192d4477a2a3f3362d6ae4fc3a7b1beb2461c85
Daniel Zdancewicz <d.zdancewicz@teb-akademia.pl>
2022-11-05 02:04:34 +0100

    registrable

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -1,34 +1,35 @@
-import { ChangeEvent, FC, PropsWithChildren, useCallback, useId } from "react";
+import { ChangeEvent, FC, InputHTMLAttributes, PropsWithChildren, useCallback, useId } from "react";
 import s from "./TextField.module.scss";
 import { Icon } from "../Icon";
 import { IconName } from "../Icon/IconRegistry";
 import cx from "classnames";
 
-interface SearchFieldProps extends PropsWithChildren {
+interface SearchFieldProps extends PropsWithChildren, Omit<InputHTMLAttributes<HTMLInputElement>, "onChange"> {
   onChange?: (query: string, event: ChangeEvent<HTMLInputElement>) => void;
   icon?: IconName;
   className?: string;
 }
 
-export const TextField: FC<SearchFieldProps> = (props) => {
+export const TextField: FC<SearchFieldProps> = ({ onChange, icon, className, ...props }) => {
   const id = useId();
   const handleChange = useCallback(
-    (event: ChangeEvent<HTMLInputElement>) => props.onChange?.(event.target.value, event),
-    [props?.onChange],
+    (event: ChangeEvent<HTMLInputElement>) => onChange?.(event.target.value, event),
+    [onChange],
   );
 
   return (
-    <div className={cx(s.container, props?.className)}>
-      {props?.icon && (
+    <div className={cx(s.container, className)}>
+      {icon && (
         <div className={s.iconContainer}>
-          <Icon name={props.icon} className={s.icon} />
+          <Icon name={icon} className={s.icon} />
         </div>
       )}
       <input
         id={id}
         className={s.input}
+        {...props}
         type="text"
-        onChange={props?.onChange ? handleChange : undefined}
+        onChange={onChange ? handleChange : undefined}
         placeholder=" "
       />
       <label htmlFor={id} id={`${id}-label`} className={s.label}>
